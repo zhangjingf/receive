d04a761e583b3e98c850e44a79bf3848740a8270
zhangjingfeng <734546749@qq.com>
2018-11-01 16:23:16 +0800

    开发

--- pages/mine/index.js
+++ pages/mine/index.js
@@ -1,14 +1,16 @@
 // pages/myMenu/index.js
 Page({
-  data: {
-
-  },
-  onLoad: function (options) {
+  onLoad: function () {
     wx.setNavigationBarTitle({
       title: '我的',
     })
     this.setData({
       checkedStatus: 'mine'
+    })
+  },
+  goWallet: function () {
+    wx.navigateTo({
+      url: '../myWallet/index',
     })
   }
 })
---
+++ pages/myWallet/index.js
@@ -0,0 +1,12 @@
+// pages/myWallet/index.js
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+
+  },
+  onShow: function () {
+
+  }
+})
